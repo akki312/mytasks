1af7d3ca17d80998dd304da2a1a2271ad91a04ad
akki312 <akshithsistla@gmail.com>
2024-02-12 12:43:57 +0530

    .slice.js

---
+++ .concat.js
@@ -0,0 +1,4 @@
+const array1 = [1, 2];
+const array2 = [3, 4];
+const concatenated = array1.concat(array2);
+console.log(concatenated);
---
+++ .slice.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 4, 5];
+const sliced = numbers.slice(1, 3);
+console.log(sliced); 
