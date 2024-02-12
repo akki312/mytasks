90ae741c82d5fd83c186a81cea1be08c815065a8
akki312 <akshithsistla@gmail.com>
2024-02-12 12:32:20 +0530

    .filter.js

---
+++ .filter.js
@@ -0,0 +1,3 @@
+const numbers = [5, 10, 15, 20];
+const filtered = numbers.filter(num => num > 10);
+console.log(filtered); 
---
+++ .find.js
@@ -0,0 +1,3 @@
+const numbers = [10, 20, 30, 40];
+const found = numbers.find(num => num > 25);
+console.log(found); 
