d35acc093c0ba62b6a9a2a5e4ea7df2d6511f519
akki312 <akshithsistla@gmail.com>
2024-02-12 12:55:57 +0530

    .reduce.js

---
+++ .constructor.js
@@ -0,0 +1,2 @@
+const arr = [];
+console.log(arr.constructor === Array); 
---
+++ .reduce.js
@@ -0,0 +1,3 @@
+const array = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+console.log(array.reduce(reducer)); 
