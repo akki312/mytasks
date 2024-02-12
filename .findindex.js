9b76f25fe6e7d32fea65efc51669a824424e9ab2
akki312 <akshithsistla@gmail.com>
2024-02-12 12:36:28 +0530

    .sort.js

---
+++ .findindex.js
@@ -0,0 +1,3 @@
+const numbers = [5, 10, 15, 20];
+const index = numbers.findIndex(num => num === 15);
+console.log(index); 
---
+++ .sort.js
@@ -0,0 +1,3 @@
+const numbers = [1, 5, 4, 6 ];
+numbers.sort();
+console.log(numbers); 
