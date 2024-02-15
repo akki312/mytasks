ed4f69aea2efabf0c066a4125b090e6775c930be
akki312 <akshithsistla@gmail.com>
2024-02-15 11:52:56 +0530

    .slice.js

--- .slice.js
+++ .slice.js
@@ -1,3 +1,5 @@
+/**slice:The .slice() method in JavaScript is used to extract a section of an array and returns
+ a new array containing the extracted elements. */
 const numbers = [1, 2, 3, 4, 5];
 const sliced = numbers.slice(1, 3);
 console.log(sliced); 
