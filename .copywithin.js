9bffd4724e70708896ff565677209cd1508a5d61
akki312 <akshithsistla@gmail.com>
2024-02-19 12:14:17 +0530

    .copywithin.js

--- .copywithin.js
+++ .copywithin.js
@@ -1,16 +1,18 @@
 /**.copyWithin() call rotates the array to the left by 3 positions. 
  * This is achieved by copying elements starting 
  * from index 3 and pasting them starting from index 0. */
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numbers = [1, 2, 3, 4, 5];
 
-// Suppose we want to:
-// 1. Rotate the elements to the left by 3 positions
-// 2. Replace elements at indices 2 and 3 with elements at indices 7 and 8
+// copying element located at 4th index to 0th index
+numbers.copyWithin(0, 4);
 
-// Step 1: Rotate the elements to the left by 3 positions
-array.copyWithin(0, 3); // Copy elements starting from index 3 and paste them starting from index 0
+// modifies the original array
+console.log(numbers); // [ 5, 6, 3, 4, 5 ]
 
-// Step 2: Replace elements at indices 2 and 3 with elements at indices 7 and 8
-array.copyWithin(2, 7, 9); // Copy elements from index 7 to 9 (not including 9) and paste them starting from index 2
+let letters = ["a", "b", "c", "d"];
 
-console.log(array); // Output: [ 4, 5, 8, 9, 10, 6, 7, 8, 9, 10 ]
+// copying element located at 1st index to 2nd index
+letters.copyWithin(2, 1);
+
+// modifies the original array 
+console.log(letters); // [ 'a', 'b', 'b', 'c' ]
