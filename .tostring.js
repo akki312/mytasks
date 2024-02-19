7cb8edeaedf01ba9efb0b46df8814cd8fae12964
akki312 <akshithsistla@gmail.com>
2024-02-19 15:58:10 +0530

    .tostring.js

--- .tostring.js
+++ .tostring.js
@@ -1,4 +1,6 @@
-/**.tostring:it displays the whole */
+/**.tostring:it displays the whole array which has been given by the 
+ * user
+*/
 const array = [1, 2, 3];
 console.log(array.toString());
 /**output:1,2,3*/
