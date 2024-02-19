9e2b09227612166f0fdb29517f75aa03382a5ff0
akki312 <akshithsistla@gmail.com>
2024-02-19 11:37:05 +0530

    .filter.js

--- .filter.js
+++ .filter.js
@@ -1,4 +1,20 @@
-const numbers = [5, 10, 15, 20];
-const filtered = numbers.filter(num => num > 10);
-console.log(filtered); 
-/**output:[15,20]*/
+/**filter:in this function it filters the numbers and perform according to
+ * the function given by the user and it display the ouput given in a string or 
+ * a array
+ */
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+// function to check even numbers
+function checkEven(number) {
+  if (number % 2 == 0)
+    return true;
+  else
+    return false;
+}
+
+// create a new array by filter even numbers from the numbers array
+let evenNumbers = numbers.filter(checkEven);
+console.log(evenNumbers);
+
+// Output: [ 2, 4, 6, 8, 10 ]
