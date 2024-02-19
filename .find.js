23ac399ef5e3f978fa21c24c798a50701a106826
akki312 <akshithsistla@gmail.com>
2024-02-19 12:05:13 +0530

    .find.js

--- .find.js
+++ .find.js
@@ -1,4 +1,15 @@
-const numbers = [10, 20, 30, 40];
-const found = numbers.find(num => num > 25);
-console.log(found); 
-/**output:30*/
+/**find: this function will find the number given in the array or string
+ * and will display the number according to function used in the loop
+ */
+let numbers = [1, 3, 4, 9, 8];
+
+// function to check even number
+function isEven(element) {
+  return element % 2 == 0;
+}
+
+// get the first even number
+let evenNumber = numbers.find(isEven);
+console.log(evenNumber);
+
+// Output: 4
