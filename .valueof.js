b93988dc7292ba04b242a81aa0c5f7a4b6682157
akki312 <akshithsistla@gmail.com>
2024-02-21 16:57:28 +0530

    .valueof.js

--- .valueof.js
+++ .valueof.js
@@ -1,6 +1,16 @@
 /**value of : it represents the number or element which is represented in the array or a
  * particular value of that array when asked by the user
  */
-const num = new Number(42);
-console.log(num.valueOf()); 
-/**output:42*/
+const obj = {
+    value: 42,
+    valueOf: function() {
+      return this.value; // Returns the value property of the object
+    }
+  };
+  
+  console.log(obj.valueOf()); // Output: 42
+  
+  // Using the object in an addition operation
+  const result = obj + 10;
+  console.log(result); // Output: 52
+  
