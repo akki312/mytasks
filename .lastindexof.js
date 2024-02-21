73840aeee73344159653941d364763750e9ceaaa
akki312 <akshithsistla@gmail.com>
2024-02-21 11:56:36 +0530

    .lastindexof.js

--- .lastindexof.js
+++ .lastindexof.js
@@ -1,4 +1,10 @@
-/**.last index:it represents the index of the last number of the given array or string  */
+/**.last index:it represents the index of last occurence of the given element
+ * if it displays the -1 in the output then it is not represented in the arrayor
+ * string
+ */
 const numbers = [2, 5, 9, 2];
 console.log(numbers.lastIndexOf(2)); 
-/**output:3*/
+console.log(numbers.lastIndexOf(1));
+/**output:3
+ *        -1
+*/
