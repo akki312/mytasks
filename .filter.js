ac0a1c03dfca137196f469711f38ccacfc665dbe
akki312 <akshithsistla@gmail.com>
2024-02-21 11:46:12 +0530

    .filter.js

--- .filter.js
+++ .filter.js
@@ -2,19 +2,16 @@
  * the function given by the user and it display the ouput given in a string or 
  * a array
  */
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const prices = [1800, 2000, null, 3000, 5000, "Thousand", 500, 8000]
 
-
-// function to check even numbers
-function checkEven(number) {
-  if (number % 2 == 0)
-    return true;
-  else
-    return false;
+function checkPrice(element) {
+  return element > 2000 && !Number.isNaN(element);
 }
 
-// create a new array by filter even numbers from the numbers array
-let evenNumbers = numbers.filter(checkEven);
-console.log(evenNumbers);
+let filteredPrices = prices.filter(checkPrice);
+console.log(filteredPrices); // [ 3000, 5000, 8000 ]
 
-// Output: [ 2, 4, 6, 8, 10 ]
+// using arrow function
+let newPrices = prices.filter((price) => (price > 2000 && !Number.isNaN(price)));
+console.log(newPrices); // [ 3000, 5000, 8000 ]
+/**[3000, 5000, 8000] */
