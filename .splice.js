f58d508e80cecf508159f6f184b1a6c260f7435c
akki312 <akshithsistla@gmail.com>
2024-02-26 10:58:59 +0530

    .splice.js

--- .splice.js
+++ .splice.js
@@ -1,8 +1,8 @@
 /**The .splice() method in JavaScript is used to change the contents 
 of an array by removing or replacing existing elements and/or adding new elements in place */ 
-let array = ['apple', 'banana', 'cherry', 'date'];
+let array = ['abcd', 'efgh', 'jhkl', 'sdaga'];
 
 // Remove 'banana' and 'cherry' and insert 'orange' at index 1
-array.splice(1, 2, 'orange');
+array.splice(1, 2, 'akki');
 
 console.log(array); // Output: ['apple', 'orange', 'date']
