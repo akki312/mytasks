6351dbbc7f52275f09bbe5d31b4157b2d3e38963
akki312 <akshithsistla@gmail.com>
2024-02-26 11:30:39 +0530

    .pop.js

--- .pop.js
+++ .pop.js
@@ -1,7 +1,7 @@
 /**.pop:it deletes the last element of the array */
 const array = [1, 2, 3];
-const lastElement = array.pop();
-console.log(lastElement); 
+const firstelement = array.pop();
+console.log(firstelement); 
 /**output:3*/
 
 console.log(array)
