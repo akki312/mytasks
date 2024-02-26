6a17f847dc2ca9d0288cfdecbedeb9f3ac771a3d
akki312 <akshithsistla@gmail.com>
2024-02-26 12:17:45 +0530

    program2.js:

---
+++ porgram2.js
@@ -0,0 +1,20 @@
+/**Write a JavaScript program to remove a character at the specified position
+of a given string and return the new string. */
+function removeCharacterAtPosition(inputString, position) {
+    
+    if (position < 0 || position >= inputString.length) {
+        return "Invalid position";
+    }
+
+    
+    let firstPart = inputString.substring(0, position);
+    let secondPart = inputString.substring(position + 1);
+
+    
+    return firstPart + secondPart;
+}
+
+
+console.log(removeCharacterAtPosition("akshith", 5));  
+console.log(removeCharacterAtPosition("sistla", 2));  
+console.log(removeCharacterAtPosition("akshithsistla", 7));   
