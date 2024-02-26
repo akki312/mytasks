a05a78ef401f9ecc205ef709c40942500d154d57
akki312 <akshithsistla@gmail.com>
2024-02-26 15:39:26 +0530

    program10.js

--- program10.js
+++ program10.js
@@ -1,18 +1,13 @@
-
 var today = new Date();
-
 
 var previousDay = new Date(today);
 previousDay.setDate(today.getDate());
-
 
 var previousDayDate = previousDay.getDate();
 var previousDayMonth = previousDay.getMonth() + 1; 
 var previousDayYear = previousDay.getFullYear() - 1; 
 
-
-
-console.log("Previous day's year: " + previousDayYear);
-console.log("Previous day's month: " + previousDayMonth);
-console.log("Previous day's date: " + previousDayDate);
+console.log("year: " + previousDayYear);
+console.log("month: " + previousDayMonth);
+console.log("date: " + previousDayDate);
 
