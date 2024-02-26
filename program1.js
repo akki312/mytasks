9515b3279f6d5e7d4c8233eb13dd0760aefb48f1
akki312 <akshithsistla@gmail.com>
2024-02-26 12:10:23 +0530

    program1.js

---
+++ program1.js
@@ -0,0 +1,20 @@
+/**Write a JavaScript program to get the difference between a given number
+and 13, if the number is greater than 13 return double the absolute
+difference */
+function differenceFrom13(number) {
+    const diff = Math.abs(number - 13);
+    if (number > 13) {
+        return diff * 2;
+    } else {
+        return diff;
+    }
+}
+
+
+console.log(differenceFrom13(8));    
+console.log(differenceFrom13(13));  
+console.log(differenceFrom13(20));  
+/**output: 5
+ *         0
+ *         14
+ */
