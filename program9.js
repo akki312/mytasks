3a48cfab77d3eca3feeaf39384764e5c2ea6dcba
akki312 <akshithsistla@gmail.com>
2024-02-26 13:36:33 +0530

    program9.js:

--- program9.js
+++ program9.js
@@ -17,3 +17,8 @@ const array2 = [3, 4, 5, 6, 7];
 console.log("Array 1:", array1);
 console.log("Array 2:", array2);
 console.log("Arrays after removing duplicates:", removeDuplicatesFromArray(array1, array2));
+/**output:
+ * Array 1: (5) [1, 2, 3, 4, 5]
+   Array 2: (5) [3, 4, 5, 6, 7]
+   Arrays after removing duplicates: (7) [1, 2, 3, 4, 5, 6, 7]
+ */
