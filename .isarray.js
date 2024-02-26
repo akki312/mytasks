36e3dcec0a225940e08a1baf9e17c60a7823d304
akki312 <akshithsistla@gmail.com>
2024-02-26 11:23:50 +0530

    .isarray.js

--- .isarray.js
+++ .isarray.js
@@ -1,3 +1,8 @@
 /**isarray:it is the function to check whether a array is true or false */
-console.log(Array.isArray([1, 2, 3]));
-/**ouput:true*/
+let array1 = [1, 2, 3];
+let array2 = ['abcd', 'efgh', 'ijkl'];
+let notAnArray = 'Hello, World!';
+
+console.log(Array.isArray(array1)); // Output: true
+console.log(Array.isArray(array2)); // Output: true
+console.log(Array.isArray(notAnArray)); // Output: false
