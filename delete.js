81940bc02abae56411761cd19e408da1d3d267ef
akki312 <akshithsistla@gmail.com>
2024-02-28 14:59:22 +0530

    delete.js

--- delete.js
+++ delete.js
@@ -1,5 +1,5 @@
 /**delete:it The delete operator is used to remove a property from an object. */
 let person = {name: "akshith", age: 23, city: "guntur"};
-delete person.city;
+delete person.name;
 console.log(person); 
 /**output:name:akshith age:23*/
