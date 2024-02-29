7a647b1d38b58fa5e01027ad3737efb82e13c72e
akki312 <akshithsistla@gmail.com>
2024-02-29 16:14:12 +0530

    example.js

--- example.js
+++ example.js
@@ -1,4 +1,5 @@
 let v = ["akshith", "sistla", "abcd", "aksh"];
+var shifted = v.shift();
 let b = {person: "akshith", gender: "male"}
 let a = [7, 8, 9, 1, 3];
 const concatenated = v.concat(a);
@@ -31,3 +32,8 @@ let new_arr = v.slice(1, 2);
 console.log(new_arr);
 delete b.gender;
 console.log(b); 
+
+console.log(shifted); 
+a.forEach(element => {
+    console.log(element);
+})
