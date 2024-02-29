3c4592842ea62776091c6cbba930976a8f58cbba
akki312 <akshithsistla@gmail.com>
2024-02-29 16:07:17 +0530

    examples.js

--- example.js
+++ example.js
@@ -1,4 +1,5 @@
 let v = ["akshith", "sistla", "abcd", "aksh"];
+let b = {person: "akshith", gender: "male"}
 let a = [7, 8, 9, 1, 3];
 const concatenated = v.concat(a);
 v.splice(1, 2, 'ledd');
@@ -9,3 +10,24 @@ a.sort();
 console.log(a);
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 console.log(a.reduce(reducer)); 
+v.push("files")
+console.log(v);
+v.pop("files")
+console.log(v);
+v.reverse();
+console.log(v);
+v.unshift("pop");
+console.log(v);
+let lengthOfArray = v.length;
+console.log(lengthOfArray)
+console.log(a.join());
+const doubled = a.map(num => num * 2);
+console.log(doubled); 
+const iterator = a.keys();
+console.log(iterator.next()); 
+console.log(iterator.next()); 
+console.log(iterator.next());
+let new_arr = v.slice(1, 2);
+console.log(new_arr);
+delete b.gender;
+console.log(b); 
