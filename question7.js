33348ccb84f404e3ea9a031db331a5ea59a6d547
akki312 <akshithsistla@gmail.com>
2024-03-04 19:07:38 +0530

    question7.js

--- question7.js
+++ question7.js
@@ -1,15 +1,13 @@
 // Write a Javascript program to find Factorial of a number 
 function factorial(n) {
-    
     if (n === 0 || n === 1) {
         return 1;
-    } 
-    else {
+    } else {
         return n * factorial(n - 1);
     }
 }
 
-
 const num = 5;
 const result = factorial(num);
-console.log(`The factorial of ${num} is: ${result}`);
+console.log("The factorial of " + num + " is: " + result);
+
