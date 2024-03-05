ca8c690bf51776fc86cae345d0cd796b0bf0be55
akki312 <akshithsistla@gmail.com>
2024-03-05 10:44:41 +0530

    question4.js

--- question4.js
+++ question4.js
@@ -1,17 +1,17 @@
 // Write a JavaScript function that accepts a string as a parameter and finds the longest word within the string.
 //Example string : 'Web Development Tutorial'
 //Expected Output : 'Development'
-function findLongestWord(str) {
-    let words = str.split(" ");
-    let longestWord = "";
-    let maxLength = 0;
-     for (let i = 0; i < words.length; i++) {
-         if (words[i].length > maxLength) {
-                longestWord = words[i];
-            maxLength = words[i].length;
-        }
+let str = 'Web Development Tutorial';
+let words = str.split(" ");
+let longestWord = "";
+let maxLength = 0;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+        longestWord = words[i];
+        maxLength = words[i].length;
     }
- return longestWord;
 }
-let exampleString = 'Web Development Tutorial';
-console.log(findLongestWord(exampleString)); 
+
+console.log(longestWord);
+
