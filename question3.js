0e489d0144fc10acfadd5b9116e1ea702d889943
akki312 <akshithsistla@gmail.com>
2024-03-05 15:37:52 +0530

    question3.js

--- question3.js
+++ question3.js
@@ -1,9 +1,5 @@
 //Write a JavaScript function that accepts a string 
 //as a parameter and converts the first letter of each word into upper case.
 let v = "the quick brown fox";
-let words = v.split(" ");
-for (let i = 0; i < words.length; i++) {
-    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-}
-let string = words.join(" ");
+let string = v.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
 console.log(string);
