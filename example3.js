fc90cccb14a8f5e973b6e242b242e55dbf5cf083
akki312 <akshithsistla@gmail.com>
2024-03-06 16:42:28 +0530

    example3.js

--- example3.js
+++ example3.js
@@ -2,7 +2,7 @@
 function sumOfSquares(numbers) {
     let sum = 0;
     for (let i = 0; i < numbers.length; i++) {
-        sum += numbers[i] * numbers[i] * numbers[i]; 
+        sum += numbers[i] * numbers[i]; 
     }
     return sum;
 }
