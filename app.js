8ace46caa9f64de6ac290f4030bfb4dd8470574a
akki312 <akshithsistla@gmail.com>
2024-04-02 11:27:49 +0530

    app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ async function connectToMongoDB() {
 
         
         const collection = db.collection('mycollection');
-        await collection.insertOne({ name: "John", age: 30 });
+        await collection.insertOne({ name: "akshith", age: 24 });
 
         console.log("Document inserted successfully");
 
