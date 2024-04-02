53e81cdcd4c264ab58b221bbcdd79d3fd4b1e21a
akki312 <akshithsistla@gmail.com>
2024-04-02 12:49:21 +0530

    app.js

--- app.js
+++ app.js
@@ -1,40 +1,23 @@
-
 const { MongoClient } = require('mongodb');
 
-
 const uri = "mongodb://localhost:27017"; // MongoDB default URI
-
-
 const dbName = "mydatabase";
-
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+const client = new MongoClient(uri);
 
 async function connectToMongoDB() {
     try {
-    
         await client.connect();
-
         console.log("Connected to MongoDB");
-
-      
         const db = client.db(dbName);
-
-        
         const collection = db.collection('mycollection');
         await collection.insertOne({ name: "akshith", age: 30 });
-
         console.log("Document inserted successfully");
-
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     } finally {
-        
         await client.close();
         console.log("Disconnected from MongoDB");
     }
 }
-
 
 connectToMongoDB();
