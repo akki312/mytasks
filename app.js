46881851e3caf064fbfeab63c62e3bcb22ea2e18
akki312 <akshithsistla@gmail.com>
2024-04-03 15:43:18 +0530

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = "mongodb+srv://akshithsistla:<password>@cluster0.iljkeyx.mongodb.net/"; // MongoDB default URI
+const uri = "mongodb://localhost:27017c"; // MongoDB default URI
 const dbName = "mydatabase";
 const client = new MongoClient(uri);
 
