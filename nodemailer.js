9d5b55f7926281718cb6545990f930e83dd5cbf7
akki312 <akshithsistla@gmail.com>
2024-04-04 11:21:52 +0530

    nodemailer.js

--- nodemailer.js
+++ nodemailer.js
@@ -1,55 +1,24 @@
 const nodemailer = require('nodemailer');
-const { MongoClient } = require('mongodb');
 
-// MongoDB connection URI
-const mongoURI = 'mongodb+srv://akshithsistla:<password>@cluster0.iljkeyx.mongodb.net/mycollection';
-const collectionName = 'emails';
-
-// Create a MongoDB client
-const mongoClient = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Create a Nodemailer transporter
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'akshithsistla@gmail.com',
-    pass: 'Akki@8008'
-  }
+let mailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'akshithsistla@gmail.com',
+        pass: 'Akki@8008'
+    }
 });
 
-// Connect to MongoDB and send email
-async function sendEmail(recipient, subject, message) {
-  try {
-    // Connect to MongoDB
-    await mongoClient.connect();
-    console.log('Connected to MongoDB');
+let mailDetails = {
+    from: 'akshithsistla@gmail.com',
+    to: 'sistlaakshith@gmail.com',
+    subject: 'Test mail',
+    text: 'testing mail'
+};
 
-    // Get the database and collection
-    const db = mongoClient.db();
-    const collection = db.collection(collectionName);
-
-    // Insert email data into MongoDB
-    await collection.insertOne({ recipient, subject, message });
-    console.log('Email data inserted into MongoDB');
-
-    // Send email using Nodemailer
-    const mailOptions = {
-      from: 'akshithsistla@gmail.com',
-      to: recipient,
-      subject: subject,
-      text: message
-    };
-
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Email sent:', info.messageId);
-  } catch (error) {
-    console.error('Error:', error);
-  } finally {
-    // Close the MongoDB connection
-    await mongoClient.close();
-    console.log('Disconnected from MongoDB');
-  }
-}
-
-// Example usage
-sendEmail('recipient@example.com', 'Test Email', 'Hello from Nodemailer and MongoDB!');
+mailTransporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+        console.error('Error Occurs:', err);
+    } else {
+        console.log('Email sent successfully');
+    }
+});
