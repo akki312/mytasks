ce6b49cbd9b678456123767fa0b3f8d6d0b9ed38
akki312 <akshithsistla@gmail.com>
2024-04-16 15:16:52 +0530

    nodemailer4.js

--- nodemailer4.js
+++ nodemailer4.js
@@ -1,41 +1,30 @@
 const twilio = require('twilio');
 const { MongoClient } = require('mongodb');
 
-
 const accountSid = 'ACd162eafc1667948933290a452b4c55be';
 const authToken = '33eb1078378fafa1bce6cf501b1e8b80';
 const twilioPhoneNumber = '+12513090858';
 
-
 const client = twilio(accountSid, authToken);
-
-
 function generateOTP() {
     return Math.floor(1000 + Math.random() * 9000); 
 }
-
-
 async function sendOTPBySMSAndStoreInMongo(phoneNumber, otp) {
-    
     client.messages
         .create({
             body: `Your OTP is: ${otp}`,
             from: twilioPhoneNumber,
-            to: phoneNumber
+            to: '918008133369'
         })
         .then(async message => {
             console.log('SMS sent:', message.sid);
 
-          
             const uri = 'mongodb+srv://akshithsistla:ccipnWsoxp5NQ0nm@cluster0.iljkeyx.mongodb.net/'; // MongoDB connection URI
             const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
             try {
-               
                 await client.connect();
                 console.log('Connected to MongoDB');
-
-               
                 const database = client.db('smsdatabase');
                 const collection = database.collection('otpCollection');
                 await collection.insertOne({ phoneNumber, otp });
@@ -43,7 +32,6 @@ async function sendOTPBySMSAndStoreInMongo(phoneNumber, otp) {
             } catch (error) {
                 console.error('Error storing OTP in MongoDB:', error);
             } finally {
-               
                 await client.close();
                 console.log('Disconnected from MongoDB');
             }
