8da8b62ccde7141e01a1052155f2846bb3115965
akki312 <akshithsistla@gmail.com>
2024-04-22 11:17:50 +0530

    app.js

---
+++ movieticketsystem/models/routes/movies.js
@@ -0,0 +1,26 @@
+// routes/movies.js
+
+const express = require('express');
+const router = express.Router();
+const Movie = require('../models/movie');
+
+router.get('/', async (req, res) => {
+    try {
+        const movies = await Movie.find();
+        res.render('movies/index', { movies });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const movie = new Movie(req.body);
+        await movie.save();
+        res.redirect('/movies');
+    } catch (error) {
+        res.status(400).json({ error: 'Invalid request body' });
+    }
+});
+
+module.exports = router;
